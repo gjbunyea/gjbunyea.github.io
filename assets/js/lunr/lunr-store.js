858300704d8b7551f01953ae0720200e31f6659c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-23 20:18:55 +0000

    Deploy gjbunyea/gjbunyea.github.io to github.com/gjbunyea/gjbunyea.github.io.git:master

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "Test Portfolio",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "http://gregbunyea.com/portfolio/test/",
+        "url": "https://gregbunyea.com/portfolio/test/",
         "teaser":null},{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
-        "url": "http://gregbunyea.com/jekyll/update/welcome-to-jekyll/",
+        "url": "https://gregbunyea.com/jekyll/update/welcome-to-jekyll/",
         "teaser":null}]
