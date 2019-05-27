b4d16370754b2d0cc0157370945d67a0aaf58105
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-27 03:21:29 +0000

    Deploy gjbunyea/gjbunyea.github.io to github.com/gjbunyea/gjbunyea.github.io.git:master

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,11 +1,21 @@
 var store = [{
-        "title": "Portfolio",
-        "excerpt":" ","categories": [],
+        "title": "METALS Capstone",
+        "excerpt":"Description    ","categories": [],
         "tags": [],
-        "url": "https://gregbunyea.com/portfolio/",
-        "teaser":null},{
+        "url": "https://gregbunyea.com/portfolio/capstone/",
+        "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
+        "title": "Debugging Tutor",
+        "excerpt":"Description    ","categories": [],
+        "tags": [],
+        "url": "https://gregbunyea.com/portfolio/debuggingtutor/",
+        "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
+        "title": "ShamSlam",
+        "excerpt":"Description    ","categories": [],
+        "tags": [],
+        "url": "https://gregbunyea.com/portfolio/shamslam/",
+        "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
         "title": "Zipper",
-        "excerpt":" ","categories": [],
+        "excerpt":"Description Zipper is an application designed to help project-based learning (PBL) instructors assess students’ skills and process growth. Zipper is the culmination of a semester-long design process, done as an academic project in Marti Louw’s Learning Media Design course as part of my METALS curriculum. The project’s initial prompt was...","categories": [],
         "tags": [],
         "url": "https://gregbunyea.com/portfolio/zipper/",
         "teaser":"https://gregbunyea.com/assets/images/zipper/zipper_display_square.png"},{
