8449f3d75771f058a925bf4f05d2387d408e4df5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-29 02:34:26 +0000

    Deploy gjbunyea/gjbunyea.github.io to github.com/gjbunyea/gjbunyea.github.io.git:master

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,24 +1,24 @@
 var store = [{
         "title": "METALS Capstone",
-        "excerpt":"Description    ","categories": [],
+        "excerpt":"Description As our METALS Capstone project, my team and I work with Renaissance Learning, Inc. to research and develop a dashboard enabling school districts to compare their student’s achievement to similar schools. We have spent Spring 2019 researching and ideating with district administrators across the country. This project is currently...","categories": [],
         "tags": [],
-        "url": "https://gregbunyea.com/portfolio/capstone/",
-        "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
-        "title": "Debugging Tutor",
-        "excerpt":"Description    ","categories": [],
-        "tags": [],
-        "url": "https://gregbunyea.com/portfolio/debuggingtutor/",
+        "url": "https://gregbunyea.com/portfolio/01-capstone/",
         "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
         "title": "ShamSlam",
-        "excerpt":"Description    ","categories": [],
+        "excerpt":"Description ShamSlam is a table-top mental-health game that aims to teach players best coping strategies for Impostor Syndrome. It was initially developed for Design for Educational Games, a course taught by Erik Harpstead at CMU. We were inspired to create a game about Impostor Syndrome as we had each personally...","categories": [],
         "tags": [],
-        "url": "https://gregbunyea.com/portfolio/shamslam/",
+        "url": "https://gregbunyea.com/portfolio/02-shamslam/",
         "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
         "title": "Zipper",
         "excerpt":"Description Zipper is an application designed to help project-based learning (PBL) instructors assess students’ skills and process growth. Zipper is the culmination of a semester-long design process, done as an academic project in Marti Louw’s Learning Media Design course as part of my METALS curriculum. The project’s initial prompt was...","categories": [],
         "tags": [],
-        "url": "https://gregbunyea.com/portfolio/zipper/",
+        "url": "https://gregbunyea.com/portfolio/03-zipper/",
         "teaser":"https://gregbunyea.com/assets/images/zipper/zipper_display_square.png"},{
+        "title": "Debugging Tutor",
+        "excerpt":"Description Debugging Tutor is an intelligent tutor that breaks down debugging into several steps. As computer scientists, all three of us felt that debugging is poorly taught, often focusing on how to debug a specific item (loop for example). The tutor is the result of research to better understand how...","categories": [],
+        "tags": [],
+        "url": "https://gregbunyea.com/portfolio/04-debuggingtutor/",
+        "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
