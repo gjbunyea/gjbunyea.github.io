542ef5877f97fa58cee48632b784b0e689519d08
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-01 22:22:31 +0000

    Deploy gjbunyea/gjbunyea.github.io to github.com/gjbunyea/gjbunyea.github.io.git:master

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -5,7 +5,7 @@ var store = [{
         "url": "https://gregbunyea.com/portfolio/01-capstone/",
         "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
         "title": "ShamSlam",
-        "excerpt":"Description ShamSlam is a table-top mental-health game that aims to teach players best coping strategies for Impostor Syndrome. It was initially developed for Design for Educational Games, a course taught by Erik Harpstead at CMU. We were inspired to create a game about Impostor Syndrome as we had each personally...","categories": [],
+        "excerpt":"Description ShamSlam is a table-top mental-health game that aims to teach players best coping strategies for Impostor Syndrome. It was initially developed for Design for Educational Games, a course taught by Erik Harpstead at CMU. We were inspired to create a game about Impostor Syndrome based on our own personal...","categories": [],
         "tags": [],
         "url": "https://gregbunyea.com/portfolio/02-shamslam/",
         "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
@@ -18,6 +18,16 @@ var store = [{
         "excerpt":"Description Debugging Tutor is an intelligent tutor that breaks down debugging into several steps. As computer scientists, all three of us felt that debugging is poorly taught, often focusing on how to debug a specific item (loop for example). The tutor is the result of research to better understand how...","categories": [],
         "tags": [],
         "url": "https://gregbunyea.com/portfolio/04-debuggingtutor/",
+        "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
+        "title": "PulmonaryPal",
+        "excerpt":"Description PulmonaryPal is a notebook designed for COPD patients in Pulmonary Rehabilitation (PR). We envision PulmonaryPal as an integral part of PR, acting as a daily health tracker as well as scaffolding key learnings. This project was completed as part of Stacie Rohrbach’s Learner Experience Design course at Carnegie Mellon’s...","categories": [],
+        "tags": [],
+        "url": "https://gregbunyea.com/portfolio/05-pulmonarypal/",
+        "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
+        "title": "Career Oriented ESL",
+        "excerpt":"Description I developed a 1-week curriculum for Ms. Anna Porter, who teaches ESL courses to adult refugees. After an interview with Ms. Porter, we selected to develop a conversation-focused module that emphasizes career preparation—a particular interest of her students. This module was developed for Educational Goals, Instruction and Assessment, a...","categories": [],
+        "tags": [],
+        "url": "https://gregbunyea.com/portfolio/06-eslcurriculum/",
         "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
