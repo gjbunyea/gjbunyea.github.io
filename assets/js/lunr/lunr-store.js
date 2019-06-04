b882d985d55079361b1b4cd29eb5fc68db5db01b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-04 02:24:57 +0000

    Deploy gjbunyea/gjbunyea.github.io to github.com/gjbunyea/gjbunyea.github.io.git:master

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -8,7 +8,7 @@ var store = [{
         "excerpt":"Description ShamSlam is a table-top mental-health game that aims to teach players best coping strategies for Impostor Syndrome. It was initially developed for Design for Educational Games, a course taught by Erik Harpstead at CMU. We were inspired to create a game about Impostor Syndrome based on our own personal...","categories": [],
         "tags": [],
         "url": "https://gregbunyea.com/portfolio/02-shamslam/",
-        "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
+        "teaser":"https://gregbunyea.com/assets/images/shamslam/game_cards.jpg"},{
         "title": "Zipper",
         "excerpt":"Description Zipper is an application designed to help project-based learning (PBL) instructors assess students’ skills and process growth. Zipper is the culmination of a semester-long design process, done as an academic project in Marti Louw’s Learning Media Design course as part of my METALS curriculum. The project’s initial prompt was...","categories": [],
         "tags": [],
@@ -18,12 +18,12 @@ var store = [{
         "excerpt":"Description Debugging Tutor is an intelligent tutor that breaks down debugging into several steps. As computer scientists, all three of us felt that debugging is poorly taught, often focusing on how to debug a specific item (loop for example). The tutor is the result of research to better understand how...","categories": [],
         "tags": [],
         "url": "https://gregbunyea.com/portfolio/04-debuggingtutor/",
-        "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
+        "teaser":"https://gregbunyea.com/assets/images/debugging_tutor/debug_zoom.png"},{
         "title": "PulmonaryPal",
         "excerpt":"Description PulmonaryPal is a notebook designed for COPD patients in Pulmonary Rehabilitation (PR). We envision PulmonaryPal as an integral part of PR, acting as a daily health tracker as well as scaffolding key learnings. This project was completed as part of Stacie Rohrbach’s Learner Experience Design course at Carnegie Mellon’s...","categories": [],
         "tags": [],
         "url": "https://gregbunyea.com/portfolio/05-pulmonarypal/",
-        "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
+        "teaser":"https://gregbunyea.com/assets/images/pulmonarypal/pulmonary_match_zoom.jpg"},{
         "title": "Career Oriented ESL",
         "excerpt":"Description I developed a 1-week curriculum for Ms. Anna Porter, who teaches ESL courses to adult refugees. After an interview with Ms. Porter, we selected to develop a conversation-focused module that emphasizes career preparation—a particular interest of her students. This module was developed for Educational Goals, Instruction and Assessment, a...","categories": [],
         "tags": [],
