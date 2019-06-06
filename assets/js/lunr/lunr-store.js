7edbef1627e7b8c809d0aed1e6cb97f249c3e9bd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-06 22:07:50 +0000

    Deploy gjbunyea/gjbunyea.github.io to github.com/gjbunyea/gjbunyea.github.io.git:master

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -28,9 +28,4 @@ var store = [{
         "excerpt":"Description Zipper is an application designed to help project-based learning (PBL) instructors assess students’ skills and process growth. Zipper is the culmination of a semester-long design process, done as an academic project in Marti Louw’s Learning Media Design course as part of my METALS curriculum. The project’s initial prompt was...","categories": [],
         "tags": [],
         "url": "https://gregbunyea.com/portfolio/zipper/",
-        "teaser":"https://gregbunyea.com/assets/images/zipper/zipper_display_square.png"},{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "https://gregbunyea.com/jekyll/update/welcome-to-jekyll/",
-        "teaser":null}]
+        "teaser":"https://gregbunyea.com/assets/images/zipper/zipper_display_square.png"},]
