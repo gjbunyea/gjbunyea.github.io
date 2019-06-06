cd118c8fdc71d22c42b145e3579769ed43e32375
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-06 19:01:25 +0000

    Deploy gjbunyea/gjbunyea.github.io to github.com/gjbunyea/gjbunyea.github.io.git:master

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,34 +1,34 @@
 var store = [{
         "title": "METALS Capstone",
-        "excerpt":"Description As our METALS Capstone project, my team and I work with Renaissance Learning, Inc. to research and develop a dashboard enabling school districts to compare their student’s achievement to similar schools. We have spent Spring 2019 researching and ideating with district administrators across the country. This project is ongoing....","categories": [],
+        "excerpt":"METALS Capstone Description As our METALS Capstone project, my team and I work with Renaissance Learning, Inc. to research and develop a dashboard enabling school districts to compare their student’s achievement to similar schools. We have spent Spring 2019 researching and ideating with district administrators across the country. This project...","categories": [],
         "tags": [],
-        "url": "https://gregbunyea.com/portfolio/01-capstone/",
+        "url": "https://gregbunyea.com/portfolio/capstone/",
         "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
+        "title": "Debugging Tutor",
+        "excerpt":"         ","categories": [],
+        "tags": [],
+        "url": "https://gregbunyea.com/portfolio/debuggingtutor/",
+        "teaser":null},{
+        "title": "Career Oriented ESL",
+        "excerpt":"Description I developed a 1-week curriculum for Ms. Anna Porter, who teaches ESL courses to adult refugees. After an interview with Ms. Porter, we selected to develop a conversation-focused module that emphasizes career preparation—a particular interest of her students. This module was developed for Educational Goals, Instruction and Assessment, a...","categories": [],
+        "tags": [],
+        "url": "https://gregbunyea.com/portfolio/eslcurriculum/",
+        "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
+        "title": "PulmonaryPal",
+        "excerpt":"Description PulmonaryPal is a notebook designed for COPD patients in Pulmonary Rehabilitation (PR). We envision PulmonaryPal as an integral part of PR, acting as a daily health tracker as well as scaffolding key learnings. This project was completed as part of Stacie Rohrbach’s Learner Experience Design course at Carnegie Mellon’s...","categories": [],
+        "tags": [],
+        "url": "https://gregbunyea.com/portfolio/pulmonarypal/",
+        "teaser":"https://gregbunyea.com/assets/images/pulmonarypal/pulmonary_match_zoom.jpg"},{
         "title": "ShamSlam",
         "excerpt":"Description ShamSlam is a table-top mental-health game that aims to teach players best coping strategies for Impostor Syndrome. It was initially developed for Design for Educational Games, a course taught by Erik Harpstead at CMU. We were inspired to create a game about Impostor Syndrome based on our own personal...","categories": [],
         "tags": [],
-        "url": "https://gregbunyea.com/portfolio/02-shamslam/",
+        "url": "https://gregbunyea.com/portfolio/shamslam/",
         "teaser":"https://gregbunyea.com/assets/images/shamslam/game_cards.jpg"},{
         "title": "Zipper",
         "excerpt":"Description Zipper is an application designed to help project-based learning (PBL) instructors assess students’ skills and process growth. Zipper is the culmination of a semester-long design process, done as an academic project in Marti Louw’s Learning Media Design course as part of my METALS curriculum. The project’s initial prompt was...","categories": [],
         "tags": [],
-        "url": "https://gregbunyea.com/portfolio/03-zipper/",
+        "url": "https://gregbunyea.com/portfolio/zipper/",
         "teaser":"https://gregbunyea.com/assets/images/zipper/zipper_display_square.png"},{
-        "title": "Debugging Tutor",
-        "excerpt":"Description Debugging Tutor is an intelligent tutor that breaks down debugging into several steps. As computer scientists, all three of us felt that debugging is poorly taught, often focusing on how to debug a specific item (loop for example). The tutor is the result of research to better understand how...","categories": [],
-        "tags": [],
-        "url": "https://gregbunyea.com/portfolio/04-debuggingtutor/",
-        "teaser":"https://gregbunyea.com/assets/images/debugging_tutor/debug_zoom.png"},{
-        "title": "PulmonaryPal",
-        "excerpt":"Description PulmonaryPal is a notebook designed for COPD patients in Pulmonary Rehabilitation (PR). We envision PulmonaryPal as an integral part of PR, acting as a daily health tracker as well as scaffolding key learnings. This project was completed as part of Stacie Rohrbach’s Learner Experience Design course at Carnegie Mellon’s...","categories": [],
-        "tags": [],
-        "url": "https://gregbunyea.com/portfolio/05-pulmonarypal/",
-        "teaser":"https://gregbunyea.com/assets/images/pulmonarypal/pulmonary_match_zoom.jpg"},{
-        "title": "Career Oriented ESL",
-        "excerpt":"Description I developed a 1-week curriculum for Ms. Anna Porter, who teaches ESL courses to adult refugees. After an interview with Ms. Porter, we selected to develop a conversation-focused module that emphasizes career preparation—a particular interest of her students. This module was developed for Educational Goals, Instruction and Assessment, a...","categories": [],
-        "tags": [],
-        "url": "https://gregbunyea.com/portfolio/06-eslcurriculum/",
-        "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
         "title": "Welcome to Jekyll!",
         "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
         "tags": [],
