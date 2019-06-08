15ce2dc831083b60d33168f1944084291b3bbe43
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-08 19:43:07 +0000

    Deploy gjbunyea/gjbunyea.github.io to github.com/gjbunyea/gjbunyea.github.io.git:master

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,31 +1,16 @@
 var store = [{
-        "title": "METALS Capstone",
-        "excerpt":"METALS Capstone Description As our METALS Capstone project, my team and I work with Renaissance Learning, Inc. to research and develop a dashboard enabling school districts to compare their student’s achievement to similar schools. We have spent Spring 2019 researching and ideating with district administrators across the country. This project...","categories": [],
-        "tags": [],
-        "url": "https://gregbunyea.com/portfolio/capstone/",
-        "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
-        "title": "Debugging Tutor",
-        "excerpt":"         ","categories": [],
-        "tags": [],
-        "url": "https://gregbunyea.com/portfolio/debuggingtutor/",
-        "teaser":null},{
         "title": "Career Oriented ESL",
-        "excerpt":"Description I developed a 1-week curriculum for Ms. Anna Porter, who teaches ESL courses to adult refugees. After an interview with Ms. Porter, we selected to develop a conversation-focused module that emphasizes career preparation—a particular interest of her students. This module was developed for Educational Goals, Instruction and Assessment, a...","categories": [],
+        "excerpt":"I developed a 1-week formal and informal English module for Ms. Anna Porter, who teaches ESL courses to adult refugees. Ms. Porter piloted this module in February 2019 praising the activities designed and understanding of her course culture. Role : Instruction Designer Duration : 3 months Course : Educational Goals,...","categories": [],
         "tags": [],
         "url": "https://gregbunyea.com/portfolio/eslcurriculum/",
-        "teaser":"https://gregbunyea.com/assets/images/creative_work.jpg"},{
-        "title": "PulmonaryPal",
-        "excerpt":"Description PulmonaryPal is a notebook designed for COPD patients in Pulmonary Rehabilitation (PR). We envision PulmonaryPal as an integral part of PR, acting as a daily health tracker as well as scaffolding key learnings. This project was completed as part of Stacie Rohrbach’s Learner Experience Design course at Carnegie Mellon’s...","categories": [],
-        "tags": [],
-        "url": "https://gregbunyea.com/portfolio/pulmonarypal/",
-        "teaser":"https://gregbunyea.com/assets/images/pulmonarypal/pulmonary_match_zoom.jpg"},{
+        "teaser":null},{
         "title": "ShamSlam",
         "excerpt":"ShamSlam is a card game to teach coping strategies for Impostor Syndrome. We were inspired to create a game about Impostor Syndrome based on our own personal experiences. This project is still in development, as we prepare for submission to the Games for Change Awards 2020. Role : Researcher, Game...","categories": [],
         "tags": [],
         "url": "https://gregbunyea.com/portfolio/shamslam/",
         "teaser":null},{
         "title": "Zipper",
-        "excerpt":"Description Zipper is an application designed to help project-based learning (PBL) instructors assess students’ skills and process growth. Zipper is the culmination of a semester-long design process, done as an academic project in Marti Louw’s Learning Media Design course as part of my METALS curriculum. The project’s initial prompt was...","categories": [],
+        "excerpt":"Zipper is an application designed to help project-based learning (PBL) instructors assess students’ skills and process growth. The project’s initial prompt was to investigate portfolio practices and assessment in PBL courses at Winchester Thurston Independent School. Role : Researcher Duration : 5 months Team Members : Angie Wang, Zach Peng...","categories": [],
         "tags": [],
         "url": "https://gregbunyea.com/portfolio/zipper/",
-        "teaser":"https://gregbunyea.com/assets/images/zipper/zipper_display_square.png"},]
+        "teaser":null},]
