c92b01aae4a926dd35745ea3a4fc9806b6f61902
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-28 16:31:58 +0000

    Deploy gjbunyea/gjbunyea.github.io to github.com/gjbunyea/gjbunyea.github.io.git:master

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"Description By focusing on Formal vs. Informal English in specific career contexts, this 1-week module is designed to empower refugees with greater English skills and career outlook. The module was developed over a 3 month period in Fall 2018 following an iterative backward design process. In January 2019, the module...","categories": [],
         "tags": [],
         "url": "https://gregbunyea.com/portfolio/esl-curriculum/",
+        "teaser":null},{
+        "title": "Interviewing E-Learning Module",
+        "excerpt":"Aiming to improve instruction in Carnegie Mellon’s User-Centered Research and Evaluation course (UCRE), we designed a module targeting interview preparation – an important, but missing piece of UCRE’s interviewing module. This skills gap became evident to us when an SME said, “Everyone agrees preparation for an interview is important. I’m...","categories": [],
+        "tags": [],
+        "url": "https://gregbunyea.com/portfolio/interviewing-module/",
         "teaser":null},]
