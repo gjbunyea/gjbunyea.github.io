a0b7dc1c29dd6a6eb665fe659c959ac382ec455f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-28 00:28:58 +0000

    Deploy gjbunyea/gjbunyea.github.io to github.com/gjbunyea/gjbunyea.github.io.git:master

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,16 +1,6 @@
 var store = [{
         "title": "Career Oriented ESL",
-        "excerpt":"I developed a 1-week formal and informal English module for Ms. Anna Porter, who teaches ESL courses to adult refugees. Ms. Porter piloted this module in February 2019 praising the activities designed and understanding of her course culture. Role : Instruction Designer Duration : 3 months Course : Educational Goals,...","categories": [],
+        "excerpt":"Description By focusing on Formal vs. Informal English in specific career contexts, this 1-week module is designed to empower refugees with greater English skills and career outlook. The module was developed over a 3 month period in Fall 2018 following an iterative backward design process. In January 2019, the module...","categories": [],
         "tags": [],
-        "url": "https://gregbunyea.com/portfolio/eslcurriculum/",
-        "teaser":null},{
-        "title": "ShamSlam",
-        "excerpt":"ShamSlam is a card game to teach coping strategies for Impostor Syndrome (IS). We were inspired to create a game about IS based on our own personal experiences. This project is still in development, as we prepare for submission to the Games for Change Awards 2020. Role : Researcher, Game...","categories": [],
-        "tags": [],
-        "url": "https://gregbunyea.com/portfolio/shamslam/",
-        "teaser":null},{
-        "title": "Zipper",
-        "excerpt":"Zipper is an application designed to help project-based learning (PBL) instructors assess students’ skills and process growth. The project’s initial prompt was to investigate portfolio practices and assessment in PBL courses at Winchester Thurston Independent School. Role : Researcher Duration : 5 months Team Members : Angie Wang, Zach Peng...","categories": [],
-        "tags": [],
-        "url": "https://gregbunyea.com/portfolio/zipper/",
+        "url": "https://gregbunyea.com/portfolio/esl-curriculum/",
         "teaser":null},]
