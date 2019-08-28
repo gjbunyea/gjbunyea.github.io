2526a9209bcc246522904092f264a73ea317d107
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-28 20:29:51 +0000

    Deploy gjbunyea/gjbunyea.github.io to github.com/gjbunyea/gjbunyea.github.io.git:master

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,8 +1,13 @@
 var store = [{
         "title": "Career Oriented ESL",
-        "excerpt":"Description By focusing on Formal vs. Informal English in specific career contexts, this 1-week module is designed to empower refugees with greater English skills and career outlook. The module was developed over a 3 month period in Fall 2018 following an iterative backward design process. In January 2019, the module...","categories": [],
+        "excerpt":"By focusing on Formal vs. Informal English in specific career contexts, this 1-week module is designed to empower refugees with greater English skills and career outlook. The module was developed over a 3 month period in Fall 2018 following an iterative backward design process. In January 2019, the module was...","categories": [],
         "tags": [],
         "url": "https://gregbunyea.com/portfolio/esl-curriculum/",
+        "teaser":null},{
+        "title": "How the Internet Works Seminar",
+        "excerpt":"For 2 years, I designed, developed, taught and iterated on a large-scale seminar about How the Internet Works or internet-class for short. This was a new course when I was hired, meaning all content was produced from the ground up. My role involved: Developing in-class activities with other Teaching Assistants...","categories": [],
+        "tags": [],
+        "url": "https://gregbunyea.com/portfolio/internet-class/",
         "teaser":null},{
         "title": "Interviewing E-Learning Module",
         "excerpt":"Aiming to improve instruction in Carnegie Mellon’s User-Centered Research and Evaluation course (UCRE), we designed a module targeting interview preparation – an important, but missing piece of UCRE’s interviewing module. This skills gap became evident to us when an SME said, “Everyone agrees preparation for an interview is important. I’m...","categories": [],
