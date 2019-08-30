58895b9156f381070c36f133190223285ad43fdb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-30 16:04:23 +0000

    Deploy gjbunyea/gjbunyea.github.io to github.com/gjbunyea/gjbunyea.github.io.git:master

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,21 +1,21 @@
 var store = [{
         "title": "Debugging Tutor",
-        "excerpt":"Currently, debugging is rarely taught as its own discrete skill set. Students are expected to get better at debugging by virtue of having to debug a lot. We believe expertise in debugging follows a cohesive process with specific strategies that can be taught explicitly. To investigate debugging as a systematic...","categories": [],
-        "tags": [],
+        "excerpt":"Currently, debugging is rarely taught as its own discrete skill set. Students are expected to get better at debugging by virtue of having to debug a lot. We believe expertise in debugging follows a cohesive process with specific strategies that can be taught explicitly. To investigate debugging as a systematic...","categories": ["Learning Design"],
+        "tags": ["Project","CS Ed","Intelligent Tutor"],
         "url": "https://gregbunyea.com/portfolio/debugging-tutor/",
         "teaser":null},{
-        "title": "Career Oriented ESL",
-        "excerpt":"By focusing on Formal vs. Informal English in specific career contexts, this 1-week module is designed to empower refugees with greater English skills and career outlook. The module was developed over a 3 month period in Fall 2018 following an iterative backward design process. In January 2019, the module was...","categories": [],
-        "tags": [],
+        "title": "Career Oriented ESL Curriculum",
+        "excerpt":"By focusing on Formal vs. Informal English in specific career contexts, this 1-week module is designed to empower refugees with greater English skills and career outlook. The module was developed over a 3 month period in Fall 2018 following an iterative backward design process. In January 2019, the module was...","categories": ["Learning Design"],
+        "tags": ["Project","Curriculum Design"],
         "url": "https://gregbunyea.com/portfolio/esl-curriculum/",
         "teaser":null},{
         "title": "How the Internet Works Seminar",
-        "excerpt":"For 2 years, I designed, developed, taught and iterated on a large-scale seminar about How the Internet Works or internet-class for short. This was a new course when I was hired, meaning all content was produced from the ground up. My role involved: Developing in-class activities with other Teaching Assistants...","categories": [],
-        "tags": [],
+        "excerpt":"For 2 years, I designed, developed, taught and iterated on a large-scale seminar about How the Internet Works or internet-class for short. This was a new course when I was hired, meaning all content was produced from the ground up. My role involved: Developing in-class activities with other Teaching Assistants...","categories": ["Learning Design"],
+        "tags": ["Work","CS Ed"],
         "url": "https://gregbunyea.com/portfolio/internet-class/",
         "teaser":null},{
         "title": "Interviewing E-Learning Module",
-        "excerpt":"Aiming to improve instruction in Carnegie Mellon’s User-Centered Research and Evaluation course (UCRE), we designed a module targeting interview preparation – an important, but missing piece of UCRE’s interviewing module. This skills gap became evident to us when an SME said, “Everyone agrees preparation for an interview is important. I’m...","categories": [],
-        "tags": [],
+        "excerpt":"Aiming to improve instruction in Carnegie Mellon’s User-Centered Research and Evaluation course (UCRE), we designed a module targeting interview preparation – an important, but missing piece of UCRE’s interviewing module. This skills gap became evident to us when an SME said, “Everyone agrees preparation for an interview is important. I’m...","categories": ["Learning Design"],
+        "tags": ["Project","E-Learning"],
         "url": "https://gregbunyea.com/portfolio/interviewing-module/",
         "teaser":null},]
